f3ba9d69ce43026d0a20e96a7d67b45d5b1d2047
Matias Gilbert <Matias Gilbert>
2015-12-29 02:11:32 -0300

    LOG

---
+++ app/src/main/java/cl/hxl/smartbike/MapsActivity.java
@@ -0,0 +1,67 @@
+package cl.hxl.smartbike;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap nMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+        nMap = gMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        nMap.setMyLocationEnabled(true);
+        CameraUpdate ZoomCam = CameraUpdateFactory.zoomTo(14);
+        nMap.animateCamera(ZoomCam);
+        nMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+            public void onMyLocationChange(Location pos) {
+                // TODO Auto-generated method stub
+
+                // Extraigo la Lat y Lon del Listener
+                double lat = pos.getLatitude();
+                double lon = pos.getLongitude();
+
+                // Muevo la cámara a mi posición
+                CameraUpdate cam = CameraUpdateFactory.newLatLng(new LatLng(
+                        lat, lon));
+
+                nMap.moveCamera(cam);
+            }
+        });
+
+    }
+}
