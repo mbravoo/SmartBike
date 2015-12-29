e830ddc5fe3b2ad89c1c49ab5663cfc62503753c
Matias Gilbert <Matias Gilbert>
2015-12-29 18:04:04 -0300

    Añadido grafico generico para el consumo mensual

--- app/src/main/java/cl/hxl/smartbike/MainActivity.java
+++ app/src/main/java/cl/hxl/smartbike/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cl.hxl.smartbike.fragments.MainChartFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,6 +38,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Abrimos el primer fragment por default
+        navigationView.getMenu().getItem(0).setChecked(true);
+        switchContent(MainChartFragment.newInstance(), null);
     }
 
     @Override
@@ -74,11 +82,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_ubicacion) {
+        if (id == R.id.nav_home) {
+            switchContent(MainChartFragment.newInstance(), null);
+        } else if (id == R.id.nav_ubicacion) {
             Intent ub = new Intent(this, MapsActivity.class);
             startActivity(ub);
-        } else if (id == R.id.nav_gallery) {
-
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_logout) {
@@ -90,5 +98,34 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void switchContent(Fragment fragment, String addBackStack) {
+        switchContent(fragment, addBackStack, -1, -1);
+    }
+
+    /**
+     * Switch given fragment on default content holder and add to back stack.
+     * Use given an animationIn and animationOut for fragment transaction
+     *
+     * @param fragment     Fragment to be switched up
+     * @param addBackStack Back stack tag
+     * @param animationIn  In animation for transaction
+     * @param animationOut Out animation for transaction
+     */
+    public void switchContent(Fragment fragment, String addBackStack, int animationIn, int animationOut) {
+        try {
+            final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            if (animationIn > 0 && animationOut > 0)
+                fragmentTransaction.setCustomAnimations(animationIn, animationOut);
+
+            if (addBackStack != null)
+                fragmentTransaction.addToBackStack(addBackStack);
+            fragmentTransaction.replace(R.id.content_frame, fragment);
+            fragmentTransaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/cl/hxl/smartbike/fragments/MainChartFragment.java
@@ -0,0 +1,100 @@
+package cl.hxl.smartbike.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.hxl.smartbike.R;
+
+public class MainChartFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    LineChart chart;
+
+    public MainChartFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainChartFragment newInstance() {
+        MainChartFragment fragment = new MainChartFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View mainView = inflater.inflate(R.layout.fragment_main_chart, null);
+
+        chart = (LineChart) mainView.findViewById(R.id.chart);
+
+        chart.setPinchZoom(true);
+
+        setData(30, 100);
+
+
+        // Inflate the layout for this fragment
+        return mainView;
+    }
+
+    private void setData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "Consumo de energía durante el mes");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.RED);
+        set1.setLineWidth(1f);
+        set1.setCircleSize(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(Color.BLACK);
+//        set1.setDrawFilled(true);
+        // set1.setShader(new LinearGradient(0, 0, 0, mChart.getHeight(),
+        // Color.BLACK, Color.WHITE, Shader.TileMode.MIRROR));
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, set1);
+
+        // set data
+        chart.setData(data);
+    }
+}
